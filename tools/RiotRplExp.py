bf351b2f0d0c4e28ec7f15e6d2dfe02c0573205a
Cedric Adjih <cedric.adjih@inria.fr>
2014-07-09 16:35:41 +0000

    minor changes

---
+++ tools/RiotRplExp.py
@@ -0,0 +1,18 @@
+#---------------------------------------------------------------------------
+# Trying to automate experiments
+#
+# Cedric Adjih - Inria - 2014
+#---------------------------------------------------------------------------
+
+import argparse
+import IotlabHelper
+
+#---------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(
+    description = "Run an Riot experiment"
+)
+
+argList = parser.parse_args()
+
+#---------------------------------------------------------------------------
