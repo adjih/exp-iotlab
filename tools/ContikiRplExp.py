17ebb285e1f42a877af571002dbb84327b5ee82a
Cedric Adjih <cedric.adjih@inria.fr>
2014-07-09 15:24:56 +0200

    misc packages

---
+++ tools/ContikiRplExp.py
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------------
+# Automating the steps by O. Chambon et al:
+# https://github.com/iot-lab/iot-lab/wiki/HOWTO-use-Foren6-to-diagnose-in-realtime-your-6LoWPAN-experiment
+#
+# Cedric Adjih - Inria - 2014
+#---------------------------------------------------------------------------
+
+import argparse
+
+import IotlabHelper
+
+
+#---------------------------------------------------------------------------
+
